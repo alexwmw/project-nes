fe8ef77a28108216556a754a635bde1a855241cb
alexwmw <alexwmw@gmail.com>
2020-08-26 11:19:30 +0100

    Mapper to new class

---
+++ project-nes/Mapper.cs
@@ -0,0 +1,67 @@
+﻿using System;
+namespace project_nes
+{
+    public interface IMapper
+    {
+
+        public byte CpuRead(ushort adr);
+
+        public void CpuWrite(ushort adr, byte data);
+
+        public byte PpuRead(ushort adr);
+
+        public void PpuWrite(ushort adr, byte data);
+
+        public byte PrgBanks {set;}
+
+        public byte[] PrgRom { set; }
+
+        public byte[] ChrRom { set; }
+    }
+
+
+    public class Mapper0 : IMapper
+    {
+
+        private byte prgBanks;
+        private byte[] prgRom, chrRom;
+
+        public byte PrgBanks { set => prgBanks = value; }
+        public byte[] PrgRom { set => prgRom = value; }
+        public byte[] ChrRom { set => chrRom = value; }
+
+        public byte CpuRead(ushort adr)
+        {
+            ushort mappedAdr =
+                    adr < 0x6000 ? throw new ArgumentOutOfRangeException($"Cartridge non-CPU range read by CPU at {adr}") :
+                    adr < 0x8000 ? throw new ArgumentOutOfRangeException($"Cartridge CHR Rom range read by CPU at {adr}") :
+                    (ushort)(adr & (prgBanks == 32 ? 0x7FFF : 0x3FFF));
+            return prgRom[mappedAdr];
+        }
+
+        public void CpuWrite(ushort adr, byte data)
+        {
+            ushort mappedAdr =
+                adr < 0x6000? throw new ArgumentOutOfRangeException($"Cartridge non-CPU range read by CPU at {adr}") :
+                adr <= 0x8000 ? throw new ArgumentOutOfRangeException($"Cartridge CHR Rom range read by CPU at {adr}"):
+                (ushort)(adr & (prgBanks == 32 ? 0x7FFF : 0x3FFF));
+            prgRom[mappedAdr] = data;
+        }
+
+        public byte PpuRead(ushort adr)
+        {
+            ushort mappedAdr =
+                (adr >= 0x0000 & adr <= 0x1FFF) ? adr :
+                throw new ArgumentOutOfRangeException($"Cartridge non-PPU range read by PPU at {adr}");
+            return chrRom[mappedAdr];
+        }
+
+        public void PpuWrite(ushort adr, byte data)
+        {
+            ushort mappedAdr =
+                (adr >= 0x6000 & adr <= 0x7FFF) ? adr :
+                throw new ArgumentOutOfRangeException($"Cartridge non-PPU range read by PPU at {adr}");
+            chrRom[mappedAdr] = data;
+        }
+    }
+}
---
+++ project-nes/Palette.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using SFML.Graphics;
+
+namespace project_nes
+{
+    public class Palette : IEnumerable<Color>
+    {
+        private List<Color> colors = new List<Color>();
+
+        public IEnumerator<Color> GetEnumerator()
+            => colors.GetEnumerator();
+
+        IEnumerator IEnumerable.GetEnumerator()
+            => colors.GetEnumerator();
+
+        public void Add(byte r, byte g, byte b)
+            => colors.Add(new Color(r, g, b));
+
+        public Color this[int i]    // Indexer declaration  
+        {
+            get { return this.colors[i]; }
+            set { this.colors[i] = value; }
+        }
+
+
+        public static Palette DefaultPalette {
+            get => new Palette
+            {
+                { 84,  84,  84 },
+                {  0,  30, 116 },
+                {  8,  16, 144 },
+                { 48,   0, 136 },
+                { 68,   0, 100 },
+                { 92,   0,  48 },
+                { 84,   4,   0 },
+                { 60,  24,   0 },
+                { 32,  42,   0 },
+                {  8,  58,   0 },
+                {  0,  64,   0 },
+                {  0,  60,   0 },
+                {  0,  50,  60 },
+                {  0,   0,   0 },
+                {  0,   0,   0 },
+                {  0,   0,   0 },
+
+                { 152, 150, 152 },
+                {   8,  76, 196 },
+                {  48,  50, 236 },
+                {  92,  30, 228 },
+                { 136,  20, 176 },
+                { 160,  20, 100 },
+                { 152,  34,  32 },
+                { 120,  60,   0 },
+                {  84,  90,   0 },
+                {  40, 114,   0 },
+                {   8, 124,   0 },
+                {   0, 118,  40 },
+                {   0, 102, 120 },
+                {   0,   0,   0 },
+                {   0,   0,   0 },
+                {   0,   0,   0 },
+
+                { 236, 238, 236 },
+                {  76, 154, 236 },
+                { 120, 124, 236 },
+                { 176,  98, 236 },
+                { 228,  84, 236 },
+                { 236,  88, 180 },
+                { 236, 106, 100 },
+                { 212, 136,  32 },
+                { 160, 170,   0 },
+                { 116, 196,   0 },
+                {  76, 208,  32 },
+                {  56, 204, 108 },
+                {  56, 180, 204 },
+                {  60,  60,  60 },
+                {   0,   0,   0 },
+                {   0,   0,   0 },
+
+                { 236, 238, 236 },
+                { 168, 204, 236 },
+                { 188, 188, 236 },
+                { 212, 178, 236 },
+                { 236, 174, 236 },
+                { 236, 174, 212 },
+                { 236, 180, 176 },
+                { 228, 196, 144 },
+                { 204, 210, 120 },
+                { 180, 222, 120 },
+                { 168, 226, 144 },
+                { 152, 226, 180 },
+                { 160, 214, 228 },
+                { 160, 162, 160 },
+                {   0,   0,   0 },
+                {   0,   0,   0 }
+            };
+        }
+
+        // Parse a .pal format file
+        public static Palette FromFile(string filePath)
+        {
+            throw new NotImplementedException();
+        }
+
+
+
+    }
+}
